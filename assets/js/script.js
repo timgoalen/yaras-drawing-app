9832791dc93a138efc56b70c92ad7e46a445b486
Tim Goalen <timgoalen@hotmail.com>
2023-09-10 13:00:02 +0000

    Start colour choice functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,6 @@ const canvas = document.getElementById("canvas");
 /* Create context */
 const ctx = canvas.getContext("2d");
 
-// Get Colour blocks
-// FROM TUTORIAL:
-// Selecting all the div that has a class of clr
-let colours = document.querySelectorAll(".colour");
-// Converting NodeList to Array
-coloursArray = Array.from(colours)
-
-coloursArray.forEach(colour => {
-  colour.addEventListener("click", () => {
-      console.log("yeahyea");
-      ctx.strokeStyle = colour.dataset.colour;
-    })
-})
-
 /* Set canvas size */
 const canvasOffsetX = canvas.offsetLeft;
 const canvasOffsetY = canvas.offsetTop;
@@ -31,10 +17,24 @@ let startY;
 /* Line style */
 ctx.lineWidth = lineWidth;
 ctx.lineCap = 'round';
-ctx.strokeStyle = "rgba(255, 192, 203, 1)";
+let colour = "";
+ctx.strokeStyle = colour;
 /* Shadow style */
 ctx.shadowColor = "rgba(255, 192, 0, 0.5)";
 ctx.shadowBlur = 3;
+
+// Colour selectors
+let colourChoiceBoxes = document.querySelectorAll("[data-colour]");
+
+function colourClickHandler(event) {
+  colourChoice = event.target.dataset.colour;
+  console.log(colourChoice);
+  colour = colourChoice;
+}
+
+colourChoiceBoxes.forEach(function(box) {
+  box.addEventListener("click", colourClickHandler);
+})
 
 /* Main drawing function */
 function drawLine(e) {
